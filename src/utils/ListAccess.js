bcf0ca3d201cf59a5234ca38a4f4be70a6440a23
Raymond Jia <raymond@savvystack.com>
2021-02-12 18:18:47 -0800

    Add some utility for easily applying access policy to many fields in a list

---
+++ src/utils/ListAccess.js
@@ -0,0 +1,77 @@
+const { listenerCount } = require("bunyan");
+
+const CreateOnly = {
+  access: {
+    read: true,
+    create: true,
+    update: false,
+  },
+};
+
+const ReadOnly = {
+  access: {
+    read: true,
+    create: false,
+    update: false,
+  },
+};
+
+const UpdateOnly = {
+  access: {
+    read: true,
+    create: false,
+    update: true,
+  },
+};
+
+const useAccess = (listDef, directives) => {
+  const applyAccessPolicyToField = (fieldName, policy) => {
+    if (fieldName in listDef.fields) {
+      if (listDef.fields[fieldName].access) {
+        throw new Error(`🚫 Field "${fieldName}" already has an access directive`);
+      }
+      listDef.fields[fieldName].access = policy.access;
+    }
+  };
+
+  const applyAccessPolicy = (directive, policy) => {
+    if (typeof directive === "string") {
+      // the directive identify a field name
+      applyAccessPolicyToField(directive, policy);
+    } else if (Array.isArray(directive)) {
+      // ["fieldA", "fieldB"]
+      directive.forEach((fieldName) => applyAccessPolicyToField(fieldName, policy));
+    } else if (typeof directive === "object") {
+      // { except: ["fieldA", "fieldB"]}
+      const fieldsToExclude = directive.except || [];
+      Object.keys(listDef.fields).forEach((fieldName) => {
+        if (!fieldsToExclude.includes(fieldName)) {
+          applyAccessPolicyToField(fieldName, policy);
+        }
+      });
+    }
+  };
+
+  const checkRequiredFields = () => {
+    Object.entries(listDef.fields).forEach(([fieldName, field]) => {
+      if (field.isRequired && field.access && !field.access.create) {
+        throw new Error(`🚫 Field "${fieldName}" is required but not creatable. Consider removing "isRequired: true".`);
+      }
+    });
+  };
+
+  applyAccessPolicy(directives.createOnly, CreateOnly);
+  applyAccessPolicy(directives.readOnly, ReadOnly);
+  applyAccessPolicy(directives.updateOnly, UpdateOnly);
+
+  checkRequiredFields();
+
+  return listDef;
+};
+
+module.exports = {
+  CreateOnly,
+  ReadOnly,
+  UpdateOnly,
+  useAccess,
+};
